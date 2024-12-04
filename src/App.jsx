cf5f73b7f7eab59d71bdef77ff5187f77e12aad2
Tahir Khan <tahirtrk42@gmail.com>
2024-12-04 11:36:07 +0530

    All the initial setup for the project is done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+function App() {
+  return (
+    <div className="text-center font-extrabold my-10 text-3xl">
+      Let's go build some E-commerce Application
+    </div>
+  );
+}
+
+export default App;
